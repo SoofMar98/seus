9030ed07e4644decb71f7f57b8b0aa4b0871aea4
cheverriaw21 <cheverriawg@gmail.com>
2022-03-15 14:22:24 -0600

    modificando 1 inicio de sesio

--- src/components/InicioSesion.vue
+++ src/components/InicioSesion.vue
@@ -1,33 +1,31 @@
 <template>
-  <div class="q-ma-lg q-pa-lg justify-center">
+  <div class="q-mx-lg">
     <!-- <div class="row">
     <div class="col col-12"></div>
     </div> -->
 
     <div class="row justify-center">
-      <div class="col">
-        <q-input
-          v-model="correo"
-          label="Correo:"
-          placeholder="@ues.edu.sv"
-          style="max-width: 300px"
-        />
-      </div>
+      <q-input
+        v-model="correo"
+        label="Correo:"
+        placeholder="@ues.edu.sv"
+        style="width: 300px"
+      />
     </div>
 
-    <div class="row">
-      <div class="col">
-        <q-input
-          v-model="clave"
-          label="Clave:"
-          type="password"
-          style="max-width: 300px"
-        />
-      </div>
+    <div class="row justify-center">
+      <q-input
+        v-model="clave"
+        label="Clave:"
+        type="password"
+        style="width: 300px"
+      />
     </div>
 
-    <div class="row">
-      <q-btn color="primary" class="q-my-md glossy">INGRESAR</q-btn>
+    <div class="row justify-center">
+      <q-btn rounded color="primary" class="q-my-md glossy text-bold"
+        >INGRESAR</q-btn
+      >
     </div>
   </div>
 </template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,8 +6,8 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     /> -->
-    <div class="row">
-      <div class="col col-12 bg-secondary glossy text-white">
+    <div class="row q-mb-md bg-info text-white glossy">
+      <div class="col col-12">
         <h1 class="text-center text-bold" style="font-size: 64px">
           <q-icon name="group" style="font-size: 64px" />
           INGRESO
@@ -16,6 +16,16 @@
     </div>
 
     <inicio-sesion />
+
+    <div class="row justify-center q-my-md">
+      <a href="https://github.com/diseno2022/seus">Sitio web del Proyecto</a>
+    </div>
+
+    <div class="row justify-center q-my-md">
+      <p class="text-center q-mx-md q-my-md">
+        Bienvenidos al sistema de elecciones de la Universidad de El Salvador.
+      </p>
+    </div>
   </q-page>
 </template>
 
