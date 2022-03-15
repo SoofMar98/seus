4bb533bf932c0dfd07fbe0b49aec09ffe2c3e526
cheverriaw21 <cheverriawg@gmail.com>
2022-03-15 13:24:21 -0600

    iniciando a trabajar en el inicioSesion

---
+++ src/components/InicioSesion.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="q-ma-lg q-pa-lg justify-center">
+    <!-- <div class="row">
+    <div class="col col-12"></div>
+    </div> -->
+
+    <div class="row justify-center">
+      <div class="col">
+        <q-input
+          v-model="correo"
+          label="Correo:"
+          placeholder="@ues.edu.sv"
+          style="max-width: 300px"
+        />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <q-input
+          v-model="clave"
+          label="Clave:"
+          type="password"
+          style="max-width: 300px"
+        />
+      </div>
+    </div>
+
+    <div class="row">
+      <q-btn color="primary" class="q-my-md glossy">INGRESAR</q-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InicioSesion",
+
+  data() {
+    return {
+      correo: "",
+      clave: "",
+    };
+  },
+
+  props: {},
+});
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
+  <q-page class="">
+    <!-- flex flex-center -->
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    />
+    /> -->
+    <div class="row">
+      <div class="col col-12 bg-secondary glossy text-white">
+        <h1 class="text-center text-bold" style="font-size: 64px">
+          <q-icon name="group" style="font-size: 64px" />
+          INGRESO
+        </h1>
+      </div>
+    </div>
+
+    <inicio-sesion />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import InicioSesion from "components/InicioSesion.vue";
 
 export default defineComponent({
-  name: "PageIndex"
+  name: "PageIndex",
+  components: {
+    InicioSesion,
+  },
 });
 </script>
