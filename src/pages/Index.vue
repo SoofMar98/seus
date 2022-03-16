2cc2323b3afa427298e798dac98d386750a529e5
cheverriaw21 <cheverriawg@gmail.com>
2022-03-16 04:22:41 -0600

    corrigiendo la ubicacion de inicio de sesion en registrase

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,37 +6,17 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     /> -->
-    <div class="row q-mb-md bg-info text-white glossy">
-      <div class="col col-12">
-        <h1 class="text-center text-bold" style="font-size: 64px">
-          <q-icon name="group" style="font-size: 64px" />
-          INGRESO
-        </h1>
-      </div>
-    </div>
-
-    <inicio-sesion />
-
-    <div class="row justify-center q-my-md">
-      <a href="https://github.com/diseno2022/seus">Sitio web del Proyecto</a>
-    </div>
-
-    <div class="row justify-center q-my-md">
-      <p class="text-center q-mx-md q-my-md">
-        Bienvenidos al sistema de elecciones de la Universidad de El Salvador.
-      </p>
-    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import InicioSesion from "components/InicioSesion.vue";
+//import InicioSesion from "components/InicioSesion.vue";
 
 export default defineComponent({
   name: "PageIndex",
   components: {
-    InicioSesion,
+    //InicioSesion,
   },
 });
 </script>
--- src/pages/Registrarse.vue
+++ src/pages/Registrarse.vue
@@ -1,9 +1,46 @@
 <template>
-  <div></div>
+  <q-page class="">
+    <!-- flex flex-center -->
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    /> -->
+    <div class="row q-mb-md bg-info text-white glossy">
+      <div class="col col-12">
+        <h1 class="text-center text-bold" style="font-size: 64px">
+          <q-icon name="group" style="font-size: 64px" />
+          INGRESO
+        </h1>
+      </div>
+    </div>
+
+    <inicio-sesion />
+
+    <div class="row justify-center q-my-md">
+      <a href="https://github.com/diseno2022/seus" target="_blank"
+        >Sitio web del Proyecto</a
+      >
+    </div>
+
+    <div class="row justify-center q-my-md">
+      <p class="text-center q-mx-md q-my-md">
+        Bienvenidos al sistema de elecciones de la Universidad de El Salvador.
+      </p>
+    </div>
+  </q-page>
 </template>
 
 <script>
-export default {};
+import { defineComponent } from "vue";
+import InicioSesion from "components/InicioSesion.vue";
+
+export default defineComponent({
+  name: "PageRegistrarse",
+  components: {
+    InicioSesion,
+  },
+});
 </script>
 
 <style></style>
