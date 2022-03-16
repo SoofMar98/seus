9e31e9e7e5e6a8bfb91e50d57f82af237043c6ad
¨Diego¨ <ve18007@ues.edu.sv>
2022-03-16 16:49:53 -0600

    iniciando a trabajar en detalle del voto

---
+++ src/components/DetalleVoto.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-center">
+    <div class="row hustify-center q-gutter-sm">
+      <div class="col-12">
+        <div id="container">
+          <h1>Nombre de la votacion</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
