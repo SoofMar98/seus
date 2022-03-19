ef7ccdbf20b1ee9004b88acd199f91066affc1d9
mg17009 <100451600+hecmango@users.noreply.github.com>
2022-03-18 19:11:29 -0600

    Modificacion layout principal

--- src/layouts/principal.vue
+++ src/layouts/principal.vue
@@ -32,22 +32,7 @@
     <q-page-container>
       <router-view />
     </q-page-container>
-    <q-footer elevated class="bg-white text-white">
-      <div class="q-pa-md">
-        <div class="q-gutter-y-md">
-          <q-tabs narrow-indicator dense align="justify" class="text-primary">
-            <q-route-tab icon="home" label="Home" to="/" exact />
-            <q-route-tab icon="how_to_vote" label="Votar" to="/votar" exact />
-            <q-route-tab
-              icon="assessment"
-              label="Resultados"
-              to="/resultado"
-              exact
-            />
-          </q-tabs>
-        </div>
-      </div>
-    </q-footer>
+   
   </q-layout>
 </template>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,6 +7,22 @@
       style="width: 200px; height: 200px"
     /> -->
   </q-page>
+   <q-footer elevated class="bg-white text-white">
+      <div class="q-pa-md">
+        <div class="q-gutter-y-md">
+          <q-tabs narrow-indicator dense align="justify" class="text-primary">
+            <q-route-tab icon="home" label="Home" to="/" exact />
+            <q-route-tab icon="how_to_vote" label="Votar" to="/votar" exact />
+            <q-route-tab
+              icon="assessment"
+              label="Resultados"
+              to="/resultado"
+              exact
+            />
+          </q-tabs>
+        </div>
+      </div>
+    </q-footer>
 </template>
 
 <script>
